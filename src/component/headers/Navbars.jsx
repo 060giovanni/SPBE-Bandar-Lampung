5d299f9e43e62443ea6a9d6ea9230f2541e0a87a
Giovanni121140060 <giovannilucy7@gmail.com>
2024-07-25 10:29:47 +0700

    updated fe

---
+++ src/component/headers/Navbars.jsx
@@ -0,0 +1,178 @@
+import Cookies from 'js-cookie';
+import { useState, useEffect } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+
+import { navLinks } from '../../data/index';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { Dropdown } from 'antd';
+
+import { UserOutlined } from '@ant-design/icons';
+
+import './Navbars.css';
+
+const Navbars = () => {
+  const [changeColor, setChangeColor] = useState(false);
+  const navigate = useNavigate();
+
+  const user = Cookies.get('user') && JSON.parse(Cookies.get('user'));
+  const email = user?.email;
+  const role = user?.role;
+  const isLogin = Cookies.get('token');
+
+  const handleLogout = () => {
+    Cookies.remove('token');
+    Cookies.remove('user');
+    navigate('/login');
+  };
+
+  const handleClickItemUser = (e) => {
+    if (e.key === 'profile') navigate('/profile');
+    else if (e.key === 'dashboard') navigate('/dashboard');
+    else if (e.key === 'chat') navigate('/chat');
+    else handleLogout();
+  };
+
+  const itemsUser = [
+    role === 'admin' && {
+      key: 'dashboard',
+      label: <span>Dashboard</span>,
+    },
+    { key: 'logout', label: <span>Logout</span> },
+  ];
+
+  const changeBackgroundColor = () => {
+    if (window.scrollY > 10) {
+      setChangeColor(true);
+    } else {
+      setChangeColor(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackgroundColor();
+    window.addEventListener('scroll', changeBackgroundColor);
+  });
+
+  return (
+    <div>
+      <Navbar
+        expand='lg'
+        className={changeColor ? 'color-active' : '' + 'navbar-custom'}
+        fixed='top'
+      >
+        <Container>
+          <Navbar.Brand
+            className='navbar-brand'
+          >
+            <img src='/assets/img/logo.png' />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='text-center'>
+              {navLinks.map((link) => {
+                return (
+                  <div className='nav-link'
+                    key={link.id}
+                  >
+                    <NavLink
+                      to={link.path}
+
+                      className={({ isActive, isPending }) =>
+                        isPending ? 'pending' : isActive ? 'active' : ''
+                      }
+                      end
+                    >
+                      {link.text}
+                    </NavLink>
+                  </div>
+                );
+              })}
+              {isLogin ? (
+                <>
+                  <div className='header-container'>
+                    <Dropdown
+                      menu={{
+                        items: itemsUser,
+                        style: { width: '50%' },
+                        onClick: handleClickItemUser,
+                      }}
+                      placement='bottomLeft'
+                      arrow
+                      trigger={['click']}
+                    >
+                      <div className='user-profile'>
+                        <UserOutlined />
+                        <span>{email}</span>
+                      </div>
+                    </Dropdown>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className='nav-link'>
+                    <NavLink
+                      to='/login'
+                      className={({ isActive, isPending }) =>
+                        isPending ? 'pending' : isActive ? 'active' : ''
+                      }
+                      end
+                    >
+                      LOGIN
+                    </NavLink>
+                  </div>
+                </>
+              )}
+            </Nav>
+
+            {/* <ul className='navbar-nav ms-auto mb-2 mb-lg-0 navbar-user text-center'>
+              {isLogin ? (
+                <>
+                  <div className='header-container'>
+                    <Dropdown
+                      menu={{
+                        items: itemsUser,
+                        style: { width: '50%' },
+                        onClick: handleClickItemUser,
+                      }}
+                      placement='bottomLeft'
+                      arrow
+                      trigger={['click']}
+                    >
+                      <div className='user-profile'>
+                        <UserOutlined />
+                        <span>{email}</span>
+                      </div>
+                    </Dropdown>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <li className='nav-item'>
+                    <NavLink
+                      to='/login'
+                      className='btn btn-outline-primary rounded-3 me-2'
+                      activeClassName='active'
+                    >
+                      Login
+                    </NavLink>
+                  </li>
+                  <li className='nav-item-2'>
+                    <NavLink
+                      to='/register'
+                      className='btn btn-primary rounded-3'
+                      activeClassName='active'
+                    >
+                      Sign Up
+                    </NavLink>
+                  </li>
+                </>
+              )}
+            </ul> */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navbars;
---
+++ src/component/headers/NavbarsContact.jsx
@@ -0,0 +1,29 @@
+import { HiMail, HiPhone } from "react-icons/hi";
+import './NavbarsContact.css';
+import { Container } from "react-bootstrap";
+
+const NavbarsContact = () => {
+   return (
+      <div className="navbars-contact-background">
+         <Container>
+            <div className="navbars-contact">
+               <div className="navbars-contact-box">
+                  <div className="navbars-contact-item">
+                     <HiMail className="navbars-contact-icon" />
+                     <a href="mailto:diskominfo@bandarlampungkota.go.id" className="navbars-contact-text email-link">diskominfo@bandarlampungkota.go.id</a>
+                  </div>
+                  <div className="navbars-contact-item">
+                     <HiPhone className="navbars-contact-icon" />
+                     <p className="navbars-contact-text">(0721) - 481301</p>
+                  </div>
+               </div>
+               <p className="navbars-contact-text-large">
+                  PEMERINTAHAN KOTA BANDAR LAMPUNG
+               </p>
+            </div>
+         </Container>
+      </div>
+   );
+}
+
+export default NavbarsContact;
